921c41d8f29c74ff922a790f3bdb8855e70ca66f
paoloprogram12 <sethpsalazar@gmail.com>
2025-09-26 00:33:23 -0700

    finish valid anagram

--- ArrayAndHashing/ValidAnagrams.py
+++ ArrayAndHashing/ValidAnagrams.py
@@ -1,9 +1,23 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return True
+
+        if len(s) != len(t):
+            return False
+        
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        return countS == countT
     
 def main():
-    print("a")
+    fort = "fortnite"
+    nite = Solution()
+    result = nite.isAnagram("nite", "nite")
+    print(result)
+
 
 if __name__ == "__main__":
     main()
